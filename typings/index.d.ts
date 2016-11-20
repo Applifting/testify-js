ed943ee7429481ad90323afb5d21c72395e719bd
Vratislav Kalenda <v.kalenda@gmail.com>
2016-11-20 13:59:50 +0100

    Add typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/shelljs/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />
+/// <reference path="modules/nightwatch/index.d.ts" />
